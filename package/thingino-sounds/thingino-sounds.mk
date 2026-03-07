4819be83161885ddad997153c03bbb5fb30eeb61
iborder <iborder@139.com>
2026-03-07 23:17:28 +0800

    fix(thingino-sounds): 修复声音文件安装命令并添加目录列表检查
    移除错误的文件复制命令，改用INSTALL命令安装文件
    添加目录列表检查以验证安装过程

--- package/thingino-sounds/thingino-sounds.mk
+++ package/thingino-sounds/thingino-sounds.mk
@@ -10,10 +10,8 @@ define THINGINO_SOUNDS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/sounds
 	mkdir -p $(TARGET_DIR)/usr/share/sounds/other
 	$(INSTALL) -m 0755 -d $(TARGET_DIR)/usr/share/sounds/other
+	ls -ltr $(TARGET_DIR)/usr/share/sounds/other/
 	ls -ltr $(THINGINO_SOUNDS_PKGDIR)/files/other/
-	cp $(THINGINO_SOUNDS_PKGDIR)/files/other/* $(TARGET_DIR)/usr/share/sounds/other/
-	$(INSTALL) -m 0644 -t $(TARGET_DIR)/usr/share/sounds/other \
-		$(THINGINO_SOUNDS_PKGDIR)/files/other/*.$(THINGINO_SOUNDS_FORMAT);
 
 	# welcome message
 	if [ "$(BR2_PACKAGE_THINGINO_SOUNDS_STARTUP)" = "y" ]; then \
